a79511e4ed7d1962197e0e2f089c3dc085c288e9
Debanjan Basu <debanjanbasu2006@gmail.com>
2019-06-13 14:57:53 +1000

    updating install script

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "Please ensure that you have wifi configured"
 
 # Running sfdisk and creating the partitions
 echo "Partinioning disk now...\n"
-sfdisk /dev/sda < xps256gbssd.sfdisk
+sfdisk /dev/sda <xps256gbssd.sfdisk
 echo "Fomatting disk now...\n"
 mkfs.fat -F32 /dev/nvme0n1p1
 mkfs.ext4 /dev/nvme0n1p2 -L ROOT
@@ -30,8 +30,11 @@ mount /dev/nvme0n1p1 /mnt/boot
 
 # pacstrapping Arch Linux
 echo "Strapping on base and base-devel arch installation (some additional packages too)...\n"
-pacstrap /mnt base base-devel dialog ifplugd wpa_suppicant sudo intel-ucode git neovim
-genfstab -L /mnt >> /mnt/etc/fstab
+
+pacstrap /mnt base base-devel dialog ifplugd wpa_suppicant sudo intel-ucode git neovim \
+xf86-input-libinput xf86-video-intel systemd-boot-pacman-hook vulkan-intel ttf-font util-linux
+
+genfstab -L /mnt >>/mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
 hwclock --systohc
@@ -40,14 +43,16 @@ hwclock --systohc
 sed -i '/#en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
 
 # Add in /etc/locale.conf
-echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo LANG=en_US.UTF-8 >/etc/locale.conf
 locale-gen
 
+# Updating and creating hostnames
+echo "Updating hostname and hosts...\n"
 MYHOSTNAME = "debxps13"
 
-echo "$MYHOSTNAME" > /etc/hostname
+echo "$MYHOSTNAME" >/etc/hostname
 # Add to hosts
-cat <<EOT >> /etc/hosts
+cat <<EOT >>/etc/hosts
 127.0.0.1 localhost
 ::1 localhost
 127.0.1.1 $MYHOSTNAME.localdomain $MYHOSTNAME
@@ -59,19 +64,19 @@ echo "root:$npasswd" | chpasswd
 useradd -m -g users -G wheel debanjanbasu
 echo "debanjanbasu:$npasswd" | chpasswd
 echo "Adding custom user to sudoers...\n"
-echo 'debanjanbasu ALL=(ALL) ALL' > /etc/sudoers.d/debanjanbasu
+echo 'debanjanbasu ALL=(ALL) ALL' >/etc/sudoers.d/debanjanbasu
 
 # Boot Loader install and config
 echo "Installing the boot loader...\n"
 
 bootctl install
 
-cat <<EOT >> /boot/loader/loader.conf
+cat <<EOT >>/boot/loader/loader.conf
 default  arch
 editor   no
 EOT
 
-cat <<EOT >> /boot/loader/entries/arch.conf
+cat <<EOT >>/boot/loader/entries/arch.conf
 title Arch Linux
 linux /vmlinuz-linux
 initrd /intel-ucode.img
