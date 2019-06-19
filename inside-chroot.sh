53e1320866f7047497b63923f832afe14110e1a2
Debanjan Basu <debanjanbasu2006@gmail.com>
2019-06-19 16:30:52 +1000

    Update inside-chroot.sh

--- inside-chroot.sh
+++ inside-chroot.sh
@@ -16,7 +16,7 @@ echo LANG=en_US.UTF-8 >/etc/locale.conf
 locale-gen
 
 # Updating and creating hostnames
-echo "Updating hostname and hosts..."
+echo "Updating hostname and hosts...\n"
 
 MYHOSTNAME=debxps13
 
