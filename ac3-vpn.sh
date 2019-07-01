c30c1c9f929a7fd5dcf8ef88fc9c9f2f9670d56c
Debanjan Basu <debanjanbasu2006@gmail.com>
2019-07-01 12:10:14 +1000

    updated i3 config

---
+++ ac3-vpn.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Author : Debanjan Basu <debanjanbasu2006@gmail.com>
+# Copyright (c) Debanjan Basu
+# Script follows here:
+
+sudo openconnect --user debanjan.basu@tech --csd-user debanjanbasu --csd-wrapper ~/.cisco/csd-wrapper.sh https://syd.access.bulletproof.net
+
